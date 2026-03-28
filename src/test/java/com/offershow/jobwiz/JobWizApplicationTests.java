8f4d63786e7a2b28d91d74aa7f517e1a2a183b58
DemWhale <haopengxu1224@gmail.com>
2026-03-29 00:19:29 +0800

    feat: initProject

---
+++ src/test/java/com/offershow/jobwiz/JobWizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.offershow.jobwiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobWizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
